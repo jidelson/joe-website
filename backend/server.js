7e0fb9f44caeaeb38d84e76e118ec7b0542ff35a
jidelson <joeidelson@gmail.com>
2023-04-21 16:36:28 -0500

    cleared build

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
-  });  
+  res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+app.listen(PORT);
