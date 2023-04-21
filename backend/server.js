d2dd889f45188642c37ccfb253df202392b897c2
jidelson <joeidelson@gmail.com>
2023-04-21 15:33:41 -0500

    updated path in server

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
   });  
 
 const PORT = process.env.PORT || 5000;
