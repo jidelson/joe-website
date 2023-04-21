b874a1eff30cb83dc0aaf820011e50bff81807bf
jidelson <joeidelson@gmail.com>
2023-04-21 18:48:00 -0500

    deploy test8

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
+app.get('/api', (req, res) => {
+  res.json({
+      message: 'This is the api endpoint'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/public/', 'index.html'));
 });
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT)
