da6cfd5a4615d849b1c27073184ae7bba24a37a9
jidelson <joeidelson@gmail.com>
2023-04-21 15:00:55 -0500

    updated build script & server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-app.use(express.static(path.join(__dirname + "/public")));
+
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
