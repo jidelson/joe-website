57e4a48a4f01b00c623b06c8aab9f3b5c21df87e
jidelson <joeidelson@gmail.com>
2023-04-21 19:02:18 -0500

    added favicon back

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,11 @@ const path = require('path')
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/api', (req, res) => {
-  res.json({
-      message: 'This is the api endpoint'
-  })
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/public/', 'index.html'));
-});
-
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
+  });  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
